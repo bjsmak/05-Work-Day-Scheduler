97314638883ddb80b29fe30992c4f1dbb40ef278
Brian Smak <brian.j.smak@gmail.com>
2020-10-09 20:23:31 -0400

    functional current time, started functions for changing css properties

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 //Display current date and time at top of page
-$('#date-today h2').text(moment().format('dddd'));
+$('#currentDay').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+//Click event for saving tasks
+$('.saveBtn').click(saveTask())
+
+//Set variable for Moment JS current hours and CSS property for colors
+var currentHour = moment().hour();
+var timeId = $('#time').text();
+var time = hourFromString(hourString);
+
+//Case function for converting hours to compare to Moment JS
+function hourFromString(hourString) {
+    switch(hourString) {
+      case "8 AM": return 8;
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 PM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
+    }
+  }
+
+//If/else statement for color properties
+
+if (time < currentHour) {$('col-md-9').addClass('past');}
+else if (time = currentHour){$('col-md-9').addClass('present');}
+else {$('col-md-9').addClass('future');}
 
 //Puts events into local storage
+//$(document).ready(function(){
+   // if(!localStorage.getItem('dayPlan')) {
+      //  $(local)
+   // }
+//})
+
 // Array needed for local storage
 
 var dayPlan = {
@@ -16,3 +53,9 @@ var dayPlan = {
     '4PM': '',
     '5PM': '',
 };
+
+//Save Task Function
+
+//function saveTask() {
+
+//}
